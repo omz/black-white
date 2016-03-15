95f0a120da45b5d7f6b2f1818c12ce5255c574f1
Chris Wilson <c.m.wilson@me.com>
2016-03-15 09:16:48 +0000

    Added difficulty setting to highscore_reader

--- highscore_reader.py
+++ highscore_reader.py
@@ -1,6 +1,10 @@
 import pickle
 
-with open("bwsave.dat", "rb") as in_file:
+# Select difficulty and run script to see high score table
+
+difficulty = 2 # Change this (1-easy, 2-regular, 3-hard)
+
+with open("bwsave"+str(difficulty)+".dat", "rb") as in_file:
 	test = pickle.load(in_file)
 
 for x in sorted(test, reverse=True):
